78eb101287dffc96fd2b0e4010283819a771c098
drogonj <drogonjm@gmail.com>
2023-05-10 12:35:30 +0200

    alpha 0.5

--- main.c
+++ main.c
@@ -11,9 +11,9 @@
 #define SCREEN_WIDTH 1280
 #define WALK_SPEED 5
 #define PI 3.141592653589793
-#define RAY_RESOLUTION 5
-#define FOV 90
-#define NUM_RAYS 64;
+#define RAY_NUMBER 144
+#define FOV 90 * (PI / 180)
+#define SENSIBILITY 0.1
 
 struct	t_core
 {
@@ -21,7 +21,8 @@ struct	t_core
 	mlx_image_t		*img_map;
 	mlx_image_t		*img_dot;
 	mlx_texture_t	*texture_dot;
-	uint32_t		color;
+	uint32_t		wall_color;
+	uint32_t		ray_color;
 
 	float			playerpos[2];
 	float			playerangle;
@@ -55,7 +56,7 @@ void	draw_map(struct t_core *core)
 				{
 					for (int px = x * 64; px < SCREEN_WIDTH && px < (x * 64) + 64; px++)
 					{
-						mlx_put_pixel(core->img_map, px, py, core->color);
+						mlx_put_pixel(core->img_map, px, py, core->wall_color);
 					}
 				}
 			}
@@ -63,53 +64,41 @@ void	draw_map(struct t_core *core)
 	}
 }
 
-float	distance_between_points(float x1, float y1, float x2, float y2)
-{
-	float	dx;
-	float	dy;
-
-	dx = x2 - x1;
-	dy = y2 - y1;
-	return (sqrt(dx * dx + dy * dy));
-}
-
-
 void	engine(void *params) {
 	struct t_core *core;
 
 	core = (struct t_core *) params;
 	if (mlx_is_key_down(core->mlx, MLX_KEY_D)
-		&& map[(int) (core->playerpos[1] / 64)][(int) (core->playerpos[0] + 18 + WALK_SPEED) / 64] != 1
-		&& map[(int) ((core->playerpos[1] + 18) / 64)][(int) (core->playerpos[0] + 18 + WALK_SPEED) / 64] != 1) {
-		core->playerpos[0] += cos((core->playerangle * 180 / M_PI) + M_PI * 2) * WALK_SPEED;
-		core->playerpos[1] -= sin((core->playerangle * 180 / M_PI) + M_PI * 2) * WALK_SPEED;
+		&& map[(int) (core->playerpos[1] / 64)][(int) (core->playerpos[0] + 8 + WALK_SPEED) / 64] != 1
+		&& map[(int) ((core->playerpos[1] + 8) / 64)][(int) (core->playerpos[0] + 8 + WALK_SPEED) / 64] != 1) {
+		core->playerpos[0] += WALK_SPEED;
 	}
 	if (mlx_is_key_down(core->mlx, MLX_KEY_A)
-		&& map[(int) (core->playerpos[1] / 64)][(int) ((core->playerpos[0] - 2) - WALK_SPEED) / 64] != 1
-		&& map[(int) ((core->playerpos[1] + 18) / 64)][(int) ((core->playerpos[0] - WALK_SPEED)) / 64] != 1) {
+		&& map[(int) (core->playerpos[1] / 64)][(int) ((core->playerpos[0] - 8) - WALK_SPEED) / 64] != 1
+		&& map[(int) ((core->playerpos[1] + 8) / 64)][(int) (((core->playerpos[0] - 8) - WALK_SPEED)) / 64] != 1) {
 		core->playerpos[0] -= WALK_SPEED;
 	}
 	if (mlx_is_key_down(core->mlx, MLX_KEY_S)
-		&& map[(int) ((core->playerpos[1] + 18 + WALK_SPEED) / 64)][(int) (core->playerpos[0]) / 64] != 1
-		&& map[(int) ((core->playerpos[1] + WALK_SPEED) + 18) / 64][(int) (core->playerpos[0] + 18) / 64] != 1) {
+		&& map[(int) ((core->playerpos[1] + 8 + WALK_SPEED) / 64)][(int) (core->playerpos[0]) / 64] != 1
+		&& map[(int) ((core->playerpos[1] + WALK_SPEED) + 8) / 64][(int) (core->playerpos[0] + 8) / 64] != 1) {
 		core->playerpos[1] += WALK_SPEED;
 	}
 	if (mlx_is_key_down(core->mlx, MLX_KEY_W)
-		&& map[(int) (((core->playerpos[1] - 5) - WALK_SPEED) / 64)][(int) (core->playerpos[0]) / 64] != 1
-		&& map[(int) ((core->playerpos[1] - WALK_SPEED) / 64)][(int) (core->playerpos[0] + 16) / 64] != 1) {
+		&& map[(int) (((core->playerpos[1] + 8) - WALK_SPEED) / 64)][(int) (core->playerpos[0]) / 64] != 1
+		&& map[(int) ((core->playerpos[1] - WALK_SPEED) / 64)][(int) (core->playerpos[0] + 8) / 64] != 1) {
 		core->playerpos[1] -= WALK_SPEED;
 	}
 	if (mlx_is_key_down(core->mlx, MLX_KEY_LEFT)) {
-		core->playerangle += 0.0005;
-		if (core->playerangle > 360)
-			core->playerangle -= 360;
-		//printf("player angle: %f(degree) %f(radians)\n", core->playerangle, core->playerangle * (PI / 180));
+		core->playerangle -= SENSIBILITY;
+		if (core->playerangle < 0)
+			core->playerangle += 6.28319;
+		printf("player angle: %f(degree) %f(radians)\n", core->playerangle, core->playerangle * (PI / 180));
 	}
 	if (mlx_is_key_down(core->mlx, MLX_KEY_RIGHT)) {
-		core->playerangle -= 0.0005;
-		if (core->playerangle < 0)
-			core->playerangle += 360;
-		//printf("player angle: %f(degree) %f(radians)\n", core->playerangle, core->playerangle * (PI / 180));
+		core->playerangle += SENSIBILITY;
+		if (core->playerangle > 6.28319)
+			core->playerangle -= 6.28319;
+		printf("player angle: %f(degree) %f(radians)\n", core->playerangle, core->playerangle * (PI / 180));
 	}
 	if (mlx_is_key_down(core->mlx, MLX_KEY_ESCAPE)) {
 		mlx_close_window(core->mlx);
@@ -121,19 +110,28 @@ void	engine(void *params) {
 	core->img_dot = mlx_texture_to_image(core->mlx, core->texture_dot);
 	draw_map(core);
 
-	float ray_x = core->playerpos[0];
-	float ray_y = core->playerpos[1];
+	int		ray = 0;
+	float	start_angle = core->playerangle - (FOV / 2);
+	float	dist_beetwen_ray = FOV / RAY_NUMBER;
 
-	while (ray_x >= 0 && ray_x < MAP_WIDTH && ray_y >= 0 && ray_y < MAP_HEIGHT && map[(int)ray_x][(int)ray_y] == 0)
+	while (ray < RAY_NUMBER)
 	{
-		ray_x += cos(core->playerangle * (PI / 180));
-		ray_y += sin(core->playerangle * (PI / 180));
+		float	ray_x = core->playerpos[0];
+		float	ray_y = core->playerpos[1];
+		float	ray_angle = start_angle + (ray * dist_beetwen_ray);
+
+		while (ray_x >= 0 && ray_x < SCREEN_WIDTH && ray_y >= 0 && ray_y < SCREEN_HEIGHT && map[(int)ray_y / 64][(int)ray_x / 64] == 0)
+		{
+			ray_x += cos(ray_angle);
+			ray_y += sin(ray_angle);
+			mlx_put_pixel(core->img_map, ray_x, ray_y, core->ray_color);
+		}
+		ray++;
 	}
-	printf("rx: %f ry: %f px: %f py: %f\n", ray_x, ray_y, core->playerpos[0], core->playerpos[1]);
+	//printf("rx: %f ry: %f px: %f py: %f\n", ray_x, ray_y, core->playerpos[0], core->playerpos[1]);
 
 	mlx_image_to_window(core->mlx, core->img_map, 0, 0);
-	mlx_image_to_window(core->mlx, core->img_dot, core->playerpos[0], core->playerpos[1]);
-	mlx_image_to_window(core->mlx, core->img_dot, ray_x, ray_y);
+	mlx_image_to_window(core->mlx, core->img_dot, core->playerpos[0] - 7.5, core->playerpos[1] - 7.5);
 	core->img_map->instances[0].z = 1;
 	core->img_dot->instances[0].z = 2;
 }
@@ -147,7 +145,8 @@ int	main(void)
 	core->img_map = mlx_new_image(core->mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
 	core->texture_dot = mlx_load_png("red_dot.png");
 	core->img_dot = mlx_texture_to_image(core->mlx, core->texture_dot);
-	core->color = (150<<24) + (150<<16) + (150<<8) + 255;
+	core->wall_color = (150<<24) + (150<<16) + (150<<8) + 255;
+	core->ray_color = (255<<24) + (0<<16) + (0<<8) + 255;
 	core->playerpos[0] = 5 * 64;
 	core->playerpos[1] = 5 * 64;
 	core->playerangle = 0;
